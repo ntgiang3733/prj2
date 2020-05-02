9a53fc478fbf74c5a90fde94d92ab828c5fd8277
Nguyễn Giang <49693338+ntgiang3733@users.noreply.github.com>
2020-05-02 20:51:30 +0700

    2/5

--- project 2/app/Http/Controllers/AjaxController.php
+++ project 2/app/Http/Controllers/AjaxController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\ProductTypes;
 use App\Models\Categories;
-
+use App\Models\ProductTypes;
 class AjaxController extends Controller
 {
-    public function getProductType(Request $request) {
-        $producttype = ProductTypes::where('idCategory',$request->idCate)->get();
-        return response()->json($producttype,200);
-
+    public function getProductType(Request $request){
+    	$producttype = ProductTypes::where('idCategory',$request->idCate)->get();
+    	return response()->json($producttype,200);
     }
 }
