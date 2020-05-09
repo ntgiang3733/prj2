c6366a9ed0601f4918c9b9637c60a3b40185414e
Nguyễn Giang <49693338+ntgiang3733@users.noreply.github.com>
2020-05-09 18:45:25 +0700

    9/05

--- project 2/app/Http/Controllers/CustomerController.php
+++ project 2/app/Http/Controllers/CustomerController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Customer;
+use App\Models\Customer;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\CustomerRequest;
+use Auth;
 class CustomerController extends Controller
 {
     /**
@@ -33,9 +34,24 @@ class CustomerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CustomerRequest $request)
     {
-        //
+		if($request->ajax()){
+            $data = $request->only('email', 'phone', 'address');
+            $data['idUser'] = Auth::user()->id;
+            if($request->active == 'on'){
+                $data['active'] = 1;
+                $customer = Customer::where('idUser',Auth::user()->id)->where('active',1)->first();
+                if(!empty($customer)){
+                    $customer->active = 0;
+                    $customer->save();
+                }
+            }else{
+                $data['active'] = 0;
+            }
+            Customer::create($data);
+            return response()->json('Đã thêm địa chỉ nhận hàng thành công',200);
+        }
     }
 
     /**
